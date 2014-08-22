460f87161a99b40bf5b841428918f95ace6aac0b
xiranou <xiranou@gmail.com>
2014-08-22 19:36:02 -0400

    add menu hover function

---
+++ JavaScript/index.js
@@ -0,0 +1,12 @@
+var main = function(){
+  $(".main-menu>li").hover(
+    function(){
+      $(".sub-menu", this).css('visibility','visible');
+      },
+    function(){
+      $(".sub-menu", this).css('visibility', 'hidden');
+    }
+    );
+};
+
+$(document).ready(main);
